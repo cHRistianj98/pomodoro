1680aaf5fe97034afd65c48f0ff6ec46ea2b56fe
vanvi <kania.praca@wp.pl>
2024-05-25 09:32:19 +0200

    Create Dtos for login endpoint

---
+++ src/main/java/com/christianj98/pomodoro/dto/JwtResponseDto.java
@@ -0,0 +1,14 @@
+package com.christianj98.pomodoro.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class JwtResponseDto {
+    private String accessToken;
+}
